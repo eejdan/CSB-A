0c8f8d20f3fba87b7c536dbb3a84b0ec603008c6
eejdan <ejdan@protonmail.com>
2021-05-12 16:01:20 +0300

    second

---
+++ api.js
@@ -0,0 +1,7 @@
+console.log("Concurs Severin Bumbaru - Echipa Agape si Metanoia 2021")
+require('dotenv').config();
+
+const express = require('express');
+const cookieParser = require('cookie-parser');
+
+const app = express();
---
+++ index.js
@@ -0,0 +1,33 @@
+console.log("Concurs Severin Bumbaru - Echipa Agape si Metanoia 2021C")
+require('dotenv').config();
+
+
+const express = require('express');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+
+{
+    let proxyIp;
+    if(process.env.LOCAL==1) proxyIp = 'localhost:' + process.env.LOCALAPIPORT;
+    else proxyIp = process.env.REMOTEAPIADDR + ':' + process.env.REMOTEAPIPORT;
+    app.use('/api', proxy(proxyIp));
+}
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(express.static('public'))
+app.use(cookieParser(process.env.COOKIEPASS))
+
+app.get('/', (req, res) => {
+    return res.render('index');
+});
+
+app.get('/login', (req,res) => {
+    return res.render('login');
+});
+
+app.listen(3030, () => { console.log("Listening");})
